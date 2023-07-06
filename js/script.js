06e7e763e81511b9ca6a9aedbe3e4340fa0461fb
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-07-05 23:14:14 -0500

    Adding ancors and smooth scroll

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("js connected");
+function checkFlexGap() {
+  let flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  let isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+checkFlexGap();
